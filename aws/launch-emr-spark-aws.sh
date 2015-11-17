df2ace44554699dec6e4662a0e7b26af445e1d88
Gregory Grubbs <gregory@dynapse.com>
2015-11-17 11:43:03 -0500

    Update for EMR 4.x

--- aws/launch-emr-spark-aws.sh
+++ aws/launch-emr-spark-aws.sh
@@ -1,11 +1,23 @@
 aws emr create-cluster \
+    --name "Spark cluster" \
     --no-visible-to-all-users \
-    --name ironspanker \
-    --ami-version 3.6 \
+    --ami-version 4.0.0 \
     --instance-type m3.xlarge \
     --instance-count 4 \
     --ec2-attributes KeyName=ironcluster-se \
-    --applications Name=Hive \
-    --bootstrap-actions Name="Spark",Path=s3://support.elasticmapreduce/spark/install-spark \
+    --applications Name=Spark, Hive \
+    --bootstrap-actions Name="DMX-h",Path=s3://syncsortpocsoftware/bootstrap/bootstrap-emr-dmxh.sh \
+    --use-default-roles
+
+
+# aws emr create-cluster \
+#     --name ironspanker \
+#     --no-visible-to-all-users \
+#     --ami-version 3.6 \
+#     --instance-type m3.xlarge \
+#     --instance-count 4 \
+#     --ec2-attributes KeyName=ironcluster-se \
+#     --applications Name=Hive \
+#     --bootstrap-actions Name="Spark",Path=s3://support.elasticmapreduce/spark/install-spark \
     Name="DMX-h",Path=s3://syncsortpocsoftware/bootstrap/bootstrap-emr-dmxh.sh
 
